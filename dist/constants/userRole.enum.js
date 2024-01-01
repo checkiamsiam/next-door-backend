8e6075e14fce53c8ca449cd9ad0f7cf185278b15
Mohammad Ali <itsproali@gmail.com>
2024-01-01 21:29:13 +0600

    Delete dist directory

--- dist/constants/userRole.enum.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.userRoleEnum = void 0;
-var userRoleEnum;
-(function (userRoleEnum) {
-    userRoleEnum["admin"] = "admin";
-    userRoleEnum["customer"] = "customer";
-})(userRoleEnum = exports.userRoleEnum || (exports.userRoleEnum = {}));
